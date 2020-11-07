451bbd8112819587379e91753f2e94e892e7ee7b
sancyo <b1845958@planet.kanazawa-it.ac.jp>
2020-11-07 16:57:26 +0900

    add:リスト2-4を追加

--- chapter2/list2-4/app.py
+++ chapter2/list2-4/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
 
@@ -10,4 +9,4 @@ def index():
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='localhost')
+    app.run(host='localhost', port=5000)
